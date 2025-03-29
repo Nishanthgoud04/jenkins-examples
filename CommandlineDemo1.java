41cbcbd8b57ceb0b7b0585835c0a109447b57cf2
Nishanthgoud04 <245122737019@mvsrec.edu.in>
2025-03-29 12:31:25 +0530

    Add files via upload

---
+++ CommandlineDemo1.java
@@ -0,0 +1,16 @@
+class CommandlineDemo1
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0])
+int n2=Integer.parseInt(args[1])
+int r1=n1+n1;
+int r1=n1-n1;
+int r1=n1*n1;
+int r1=n1/n1;
+System.out.println("sum"+r1);
+System.out.println("sub"+r1);
+System.out.println("mul"+r1);
+System.out.println("div"+r1);
+}
+}
